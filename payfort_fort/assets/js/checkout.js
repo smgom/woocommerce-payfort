f391e0f3b3b6c0b0f71979c5930543ab07f065ff
Deya Zalloum <dzalloum@payfort.com>
2016-09-22 14:50:14 +0300

    new version release (1.2.0)

--- payfort_fort/assets/js/checkout.js
+++ payfort_fort/assets/js/checkout.js
@@ -1,116 +1,116 @@
-/*jQuery('form.checkout').on('submit', function (e){
-   if(jQuery('input[name=payment_method]:checked').val() == 'payfort') {
-       e.preventDefault();
-       return fortFormHandler();
-   }
-});*/
-jQuery('form.checkout').on('checkout_place_order_payfort', function () {
-    return fortFormHandler();
-});
 
-jQuery('form#order_review').on('submit', function () {
-    return fortFormHandler();
-});
 
-function initPayfortFortPayment() {
-    var data = jQuery('form.checkout, form#order_review').serialize();
-    data += '&SADAD=' + jQuery('[data-method=SADAD]').is(':checked');
-    data += '&NAPS=' + jQuery('[data-method=NAPS]').is(':checked');
-    var ajaxUrl = wc_checkout_params.checkout_url;
-//    if(jQuery('form#order_review').size() == 0){
-//        ajaxUrl = '?wc-ajax=checkout';
-//    }
-    jQuery.ajax({
-        'url': ajaxUrl,
-        'type': 'POST',
-        'dataType': 'json',
-        'data': data,
-        'async': false
-    }).complete(function (response) {
-        data = '';
-        if(response.form) {
-            data = response;
+jQuery(document).ready(function() {
+    "use strict";
+    function showError(form, data) {
+        // Remove notices from all sources
+        jQuery( '.woocommerce-error, .woocommerce-message' ).remove();
+
+        // Add new errors returned by this event
+        if ( data.messages ) {
+                form.prepend( '<div class="woocommerce-NoticeGroup-updateOrderReview">' + data.messages + '</div>' );
+        } else {
+                form.prepend( data );
         }
-        else{
-            var code = response.responseText;
-            var newstring = code.replace(/<script[^>]*>(.*)<\/script>/, "");
-            if (newstring.indexOf("<!--WC_START-->") >= 0) {
-                    newstring = newstring.split("<!--WC_START-->")[1];
-            }
-            if (newstring.indexOf("<!--WC_END-->") >= 0) {
-                    newstring = newstring.split("<!--WC_END-->")[0];
-            }
-            try {
-                data = jQuery.parseJSON( newstring );
-            }
-            catch(e) {}
+
+        // Lose focus for all fields
+        form.find( '.input-text, select, input:checkbox' ).blur();
+
+        // Scroll to top
+        jQuery( 'html, body' ).animate( {
+                scrollTop: ( jQuery( form ).offset().top - 100 )
+        }, 1000 );
+    }
+    var form = jQuery("form.checkout");
+    form.length ? (form.bind("checkout_place_order_payfort", function() {
+        return fortFormHandler(jQuery(this));
+    })) : jQuery("form#order_review").submit(function() {
+        var paymentMethod = jQuery("#order_review input[name=payment_method]:checked").val();
+        return "payfort" === paymentMethod ? fortFormHandler(jQuery(this)) : void 0;
+    });
+    
+    function fortFormHandler(form) {
+        if (form.is(".processing")) return !1;
+        return initPayfortFortPayment(form);
+    }
+    
+    function isMerchantPageMethod() {
+        if(!jQuery('[data-method=NAPS]').is(':checked') && !jQuery('[data-method=SADAD]').is(':checked')
+                && jQuery('#payfort_fort_cc_integration_type').val() == 'merchantPage') {
+            return true;
         }
-        if (data.form) {
-            jQuery('#payfort_payment_form').remove();
-            jQuery('body').append(data.form);
-            window.success = true;
-            payfortPaymentSuccess = true;
-            if(!isMerchantPageMethod()) {
-                jQuery( "#payfort_payment_form" ).submit();
+        return false;
+    }
+
+    function isMerchantPage2Method() {
+        if(!jQuery('[data-method=NAPS]').is(':checked') && !jQuery('[data-method=SADAD]').is(':checked')
+                && jQuery('#payfort_fort_cc_integration_type').val() == 'merchantPage2') {
+            return true;
+        }
+        return false;
+    }
+    
+    function initPayfortFortPayment(form) {
+        var data = jQuery(form).serialize();
+        var isSadad = jQuery('[data-method=SADAD]').is(':checked');
+        var isNAPS = jQuery('[data-method=NAPS]').is(':checked');
+        if(isMerchantPage2Method()) {
+            //validate credit card form
+            var isValid = payfortFortMerchantPage2.validateCcForm(form);
+            if(!isValid) {
+                return !1;
+            }
+        }
+        data += '&SADAD=' + isSadad;
+        data += '&NAPS=' + isNAPS;
+        var ajaxUrl = wc_checkout_params.checkout_url;
+    //    if(jQuery('form#order_review').size() == 0){
+    //        ajaxUrl = '?wc-ajax=checkout';
+    //    }
+        jQuery.ajax({
+            'url': ajaxUrl,
+            'type': 'POST',
+            'dataType': 'json',
+            'data': data,
+            'async': false
+        }).complete(function (response) {
+            data = '';
+            if(response.form) {
+                data = response;
             }
             else{
-                showMerchantPage(jQuery('#payfort_payment_form').attr('action'));
+                var code = response.responseText;
+                var newstring = code.replace(/<script[^>]*>(.*)<\/script>/, "");
+                if (newstring.indexOf("<!--WC_START-->") >= 0) {
+                        newstring = newstring.split("<!--WC_START-->")[1];
+                }
+                if (newstring.indexOf("<!--WC_END-->") >= 0) {
+                        newstring = newstring.split("<!--WC_END-->")[0];
+                }
+                try {
+                    data = jQuery.parseJSON( newstring );
+                }
+                catch(e) {}
             }
-        }
-
-    });
-}
-
-function fortFormHandler() {
-
-    var form = jQuery('form.checkout, form#order_review');
-    if (jQuery('#payment_method_payfort').is(':checked') || jQuery('[data-method=SADAD]').is(':checked') || jQuery('[data-method=NAPS]').is(':checked')) {
-        if (0 === jQuery('input[name=payfortToken]').size()) {
-            initPayfortFortPayment();
-            if (window.success) {
-                return false; //prevent showing error from default wordpress behaviour
+            if(data.result == 'failure') {
+                showError(form, data);
+                return !1;
             }
-            else {
-                return true;
+            if (data.form) {
+                jQuery('#frm_payfort_fort_payment').remove();
+                jQuery('body').append(data.form);
+                window.success = true;
+                if(isMerchantPage2Method()) {
+                    payfortFortMerchantPage2.submitMerchantPage();
+                }
+                else if(isMerchantPageMethod()) {
+                    payfortFortMerchantPage.showMerchantPage(jQuery('#frm_payfort_fort_payment').attr('action'));
+                }
+                else{                   
+                    jQuery( "#frm_payfort_fort_payment" ).submit();
+                }
             }
-        }
+        });
+        return !1;
     }
-
-    return true;
-}
-
-function isMerchantPageMethod() {
-    if(!jQuery('[data-method=NAPS]').is(':checked') && !jQuery('[data-method=SADAD]').is(':checked')
-            && jQuery('#payfort_integration_type').val() == 'merchantPage') {
-        return true;
-    }
-    return false;
-}
-
-function showMerchantPage(merchantPageUrl) {
-    if(jQuery("#payfort_merchant_page").size()) {
-        jQuery( "#payfort_merchant_page" ).remove();
-    }
-    jQuery("#review-buttons-container .btn-checkout").hide();
-    jQuery("#review-please-wait").show();
-    
-    jQuery('<iframe  name="payfort_merchant_page" id="payfort_merchant_page"height="550px" frameborder="0" scrolling="no" onload="pfIframeLoaded(this)" style="display:none"></iframe>').appendTo('#pf_iframe_content');
-    jQuery('.pf-iframe-spin').show();
-    jQuery('.pf-iframe-close').hide();
-    jQuery( "#payfort_merchant_page" ).attr("src", merchantPageUrl);
-    jQuery( "#payfort_payment_form" ).attr("action", merchantPageUrl);
-    jQuery( "#payfort_payment_form" ).attr("target","payfort_merchant_page");
-    jQuery( "#payfort_payment_form" ).submit();
-    jQuery( "#div-pf-iframe" ).show();
-}
-
-function pfClosePopup() {
-    jQuery( "#div-pf-iframe" ).hide();
-    jQuery( "#payfort_merchant_page" ).remove();
-    window.location = jQuery( "#payfort_cancel_url" ).val();
-}
-function pfIframeLoaded(ele) {
-    jQuery('.pf-iframe-spin').hide();
-    jQuery('.pf-iframe-close').show();
-    jQuery('#payfort_merchant_page').show();
-}
+});
---
+++ payfort_fort/lib/payfortFort/classes/Order.php
@@ -0,0 +1,141 @@
+<?php
+
+class Payfort_Fort_Order extends Payfort_Fort_Super
+{
+
+    private $order = array();
+    private $orderId;
+    private $pfConfig;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->pfConfig = Payfort_Fort_Config::getInstance();
+    }
+
+    public function loadOrder($orderId)
+    {
+        $this->orderId = $orderId;
+        $this->order   = $this->getOrderById($orderId);
+    }
+
+    public function setOrderId($orderId)
+    {
+        $this->orderId = $orderId;
+    }
+
+    public function setOrder($order)
+    {
+        $this->order = $order;
+    }
+
+    public function getSessionOrderId()
+    {
+        return WC()->session->get('order_awaiting_payment');
+    }
+
+    public function getOrderId()
+    {
+        return $this->order->id;
+    }
+
+    public function getOrderById($orderId)
+    {
+        $order = new WC_Order($orderId);
+        return $order;
+    }
+
+    public function getLoadedOrder()
+    {
+        return $this->order;
+    }
+
+    public function getEmail()
+    {
+        return $this->order->billing_email;
+    }
+
+    public function getCustomerName()
+    {
+        $fullName  = '';
+        $firstName = $this->order->billing_first_name;
+        $lastName  = $this->order->billing_last_name;
+
+        $fullName = trim($firstName . ' ' . $lastName);
+        return $fullName;
+    }
+
+    public function getCurrencyCode()
+    {
+        return $this->order->get_order_currency();
+    }
+
+    public function getCurrencyValue()
+    {
+        return 1;
+    }
+
+    public function getTotal()
+    {
+        return $this->order->get_total();
+    }
+
+    public function getPaymentMethod()
+    {
+        return $this->order->payment_method;
+    }
+
+    public function getStatusId()
+    {
+        return $this->order->get_status();
+    }
+
+    public function declineOrder($reason = '')
+    {
+        $status = 'failed';
+        if(!$this->getOrderId()) {
+            return true;
+        }
+        if ($this->getStatusId() == $status) {
+            return true;
+        }
+        $note = 'Payment Failed';
+        if(!empty($reason)) {
+            $note .= " ($reason)";
+        }
+        $this->order->cancel_order($note);
+        $this->order->update_status( $status, $note );
+        unset( WC()->session->order_awaiting_payment );
+        if($this->pfConfig->orderPlacementIsOnSuccess()) {
+            $this->order->update_status( '', 'Hide Order' );
+        }
+        return true;
+    }
+
+    public function cancelOrder()
+    {
+        $status = 'cancelled';
+        if(!$this->getOrderId()) {
+            return true;
+        }
+        if ($this->getStatusId() == $status) {
+            return true;
+        }
+        $this->order->cancel_order('Payment Cancelled');
+        if($this->pfConfig->orderPlacementIsOnSuccess()) {
+            $this->order->update_status( '', 'Hide Order' );
+        }
+        return true;
+    }
+
+    public function successOrder($response_params, $response_mode)
+    {
+        if ($this->getOrderId()) {
+            $this->order->payment_complete();
+        }
+        return true;
+    }
+
+}
+
+?>
